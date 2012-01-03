7c43d3e8d950fdf149b48da963ffd6aaf7d8a5b0
Marco Verdecchia <marco.verdecchia@gmail.com>
2012-01-03 17:05:13 +0100

    config.py contains all parameters for backend.

--- config.py
+++ config.py
@@ -1,2 +1,24 @@
+import ConfigParser
+
+file = 'config.ini'
+config = ConfigParser.RawConfigParser()
+config.read(file)
+
+
+class platform:
+    type = ""
+    def __init__(self):
+        self.type = config.get('PLATFORM','type')
+
 class database:
-    type = "google" #google,mysql
+    db_host = ""
+    db_port = 3306
+    db_name = ""
+    db_user = ""
+    db_password = ""
+    def __init__(self):
+        self.db_host = config.get('DATABASE','db_host')
+        self.db_port = config.getint('DATABASE','db_port')
+        self.db_name = config.get('DATABASE','db_name')
+        self.db_user = config.get('DATABASE','db_user')
+        self.db_password = config.get('DATABASE','db_password')
